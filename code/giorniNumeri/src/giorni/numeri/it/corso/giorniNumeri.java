23d2453495671c6c1f6e63dc8e1e118248030854
unknown <cpepe@studenti.apuliadigitalmaker.it>
2022-11-24 16:08:37 +0100

    giorni

---
+++ code/giorniNumeri/src/giorni/numeri/it/corso/giorniNumeri.java
@@ -0,0 +1,40 @@
+package giorni.numeri.it.corso;
+
+import java.util.Scanner;
+
+public class giorniNumeri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		String days;
+		int numbers;
+
+		numbers = scanner.nextInt();
+
+		if (numbers == 7)
+			System.out.println("domenica");
+
+		if (numbers == 6)
+			System.out.println();
+
+		if (numbers == 5)
+			System.out.println("venerdì");
+
+		if (numbers == 4)
+			System.out.println();
+
+		if (numbers == 3)
+			System.out.println();
+
+		if (numbers == 2)
+			System.out.println();
+
+		if (numbers == 1)
+			System.out.println();
+
+	}
+
+}
