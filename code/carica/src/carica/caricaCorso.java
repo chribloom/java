47ad73d7246e800691557e0e4a2d22bad9d2128e
unknown <cpepe@studenti.apuliadigitalmaker.it>
2022-11-24 16:37:29 +0100

    carica

---
+++ code/carica/src/carica/caricaCorso.java
@@ -0,0 +1,28 @@
+package carica;
+
+public class caricaCorso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 2;
+		int carica = 2;
+		
+			
+	for (int i = 0; i<15 ; i++) {
+		carica = numero * carica;
+				
+		System.out.println(carica);		
+	}
+			
+	int wa = 0;
+	carica = 2;
+	while (wa < 15) {
+		wa++;
+		carica = numero * carica;
+		System.out.println(carica);
+			
+		}
+	}
+
+}
